b002da03915feaf63a50d275226f04bedd3176db
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-09-05 15:14:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 // complete this js code
-class Person {
+export class Person {
     constructor(name, age) {
       this.name = name;
       this.age = age;
     }
-  
 
     greet() {
       console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     }
-  }
-  
-  
-  class Employee extends Person {
+}
+
+export class Employee extends Person {
     constructor(name, age, jobTitle) {
-      
       super(name, age);
       this.jobTitle = jobTitle;
     }
-  
-  
+
     jobGreet() {
       console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
-  }
+}
   
-  
-  const person = new Person('Alice', 30);
+  const person = new Person('Prince', 30);
   person.greet(); 
-  const employee = new Employee('Bob', 40, 'Software Engineer');
+  const employee = new Employee('Rakhi', 40, 'Software Engineer');
   employee.greet();     
   employee.jobGreet();  
