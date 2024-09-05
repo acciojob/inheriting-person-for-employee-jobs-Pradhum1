04389204abc84b9301e53116ecbcb58902a27bbf
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-09-05 15:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
 
-function Employee(name, age, jobTitle) {}
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+    greet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+  }
+  
+  
+  class Employee extends Person {
+    constructor(name, age, jobTitle) {
+      
+      super(name, age);
+      this.jobTitle = jobTitle;
+    }
+  
+  
+    jobGreet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+  }
+  
+  
+  const person = new Person('Alice', 30);
+  person.greet(); 
+  const employee = new Employee('Bob', 40, 'Software Engineer');
+  employee.greet();     
+  employee.jobGreet();  
