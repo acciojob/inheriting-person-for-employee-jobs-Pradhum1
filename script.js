87f498abe57e136585bccae3ffa832751716d6e2
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-09-05 15:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // complete this js code
-export class Person {
+class Person {
     constructor(name, age) {
       this.name = name;
       this.age = age;
@@ -10,7 +10,7 @@ export class Person {
     }
 }
 
-export class Employee extends Person {
+class Employee extends Person {
     constructor(name, age, jobTitle) {
       super(name, age);
       this.jobTitle = jobTitle;
@@ -20,6 +20,11 @@ export class Employee extends Person {
       console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
+
+module.exports = { Person, Employee };
+And then, in your test file, you can import these classes using require:
+
+const { Person, Employee } = require('./path_to_script_file');
   
   const person = new Person('Prince', 30);
   person.greet(); 
